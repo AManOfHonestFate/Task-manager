ab6ffa4eaf6993a3daac1737f84ea130bf624be2
Egor <khromov.developer@mail.ru>
2022-09-27 16:31:14 +0300

    selec-none

--- src/components/TodoTable.tsx
+++ src/components/TodoTable.tsx
@@ -7,7 +7,7 @@ interface TodoTableProps {
 export default function TodoTable({ title, children } : TodoTableProps) {
 
     return (
-        <div className="max-w-sm py-2 px-3 border-2 border-primary rounded cursor-pointer">
+        <div className="max-w-sm py-2 px-3 border-2 border-primary rounded cursor-pointer select-none">
             <h3 className="text-lg mb-2">{ title }</h3>
             <span className="flex flex-wrap">
                 {children.map((el, idx) => {
