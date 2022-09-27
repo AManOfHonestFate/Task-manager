ad64060f6fd81d8d39f9f97f6a474808d9eecc86
Egor <khromov.developer@mail.ru>
2022-09-27 15:55:36 +0300

    added key

--- src/components/TodoTable.tsx
+++ src/components/TodoTable.tsx
@@ -10,8 +10,8 @@ export default function TodoTable({ title, children } : TodoTableProps) {
         <div className="max-w-sm py-2 px-3 border-2 border-primary rounded cursor-pointer">
             <h3 className="text-lg mb-2">{ title }</h3>
             <span className="flex flex-wrap">
-                {children.map(el => {
-                    return <TodoMiniItem>{ el }</TodoMiniItem>
+                {children.map((el, idx) => {
+                    return <TodoMiniItem key={idx}>{ el }</TodoMiniItem>
                 })}
             </span>
         </div>
