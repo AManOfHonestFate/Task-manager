961dac100c3a994d8a1fe4e9cf4ffd726df19f97
Egor <khromov.developer@mail.ru>
2022-09-28 21:33:04 +0300

    TodoItem -> TodoSingle

---
+++ src/components/TodoSingle.tsx
@@ -0,0 +1,26 @@
+import {useState} from "react";
+
+interface TodoItemProps {
+    children: JSX.Element | string
+}
+
+export default function TodoItem({ children } : TodoItemProps) {
+    const [isDone, setIsDone] = useState(false);
+
+    function setDone() {
+        setIsDone(!isDone);
+    }
+
+    return (
+        <div className={
+            `max-w-sm py-2 px-3 border-2 rounded-xl cursor-pointer text-lg
+            ${isDone ? "text-tertiary border-tertiary line-through" : "text-primary border-primary hover:text-secondary hover:border-secondary"}`
+        }
+             onClick={setDone}
+        >
+            <span className="text-primary">
+                { children }
+            </span>
+        </div>
+    )
+}
