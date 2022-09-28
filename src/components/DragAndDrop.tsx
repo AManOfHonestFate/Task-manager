66e5e3ab8b609eeace1ef9f499e8d73047fbe810
Egor <khromov.developer@mail.ru>
2022-09-28 17:16:32 +0300

    fixed mouse move event

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,34 +1,54 @@
-import {useState, MouseEvent, useRef, RefObject, LegacyRef, useEffect} from "react";
+import {useState, MouseEvent, useRef, useEffect} from "react";
 
 interface DragAndDropProps {
-    children: JSX.Element
+    children: JSX.Element,
+    className?: string
 }
 
-export default function DragAndDrop({ children }: DragAndDropProps) {
-    const [isDragged, setIsDragged] = useState(false);
-    const [position, setPosition] = useState({x: 0, y: 0})
-    const el = useRef<HTMLSpanElement>(null);
+type MouseStates = 'none' | 'down' | 'dragged';
 
+export default function DragAndDrop({ children, className }: DragAndDropProps) {
+    const [mouseState, setMouseState] = useState('none' as MouseStates);
+    const [position, setPosition] = useState({x: 0, y: 0});
+
+    const el = useRef<HTMLSpanElement>(null);
     const elSize = useRef({width: 0, height: 0});
+
     useEffect(() => {
         if (!el.current) return;
 
         elSize.current.height = parseFloat(window.getComputedStyle(el.current).height);
-        elSize.current.width = parseFloat(window.getComputedStyle(el.current).height);
-    }, [])
+        elSize.current.width = parseFloat(window.getComputedStyle(el.current).width);
+    }, [el.current])
+
+    function mouseMoveListener (e: globalThis.MouseEvent) {
+        setPosition({x: e.clientX - elSize.current.width / 2, y: e.clientY - elSize.current.height / 2});
+    }
 
     function handleMove(e: MouseEvent) {
-        if (!isDragged) return;
+        if (mouseState !== 'down') return;
 
-        setPosition({x: e.clientX - elSize.current.width, y: e.clientY - elSize.current.height / 2})
+        setMouseState('dragged')
+        window.addEventListener('mousemove', mouseMoveListener);
+    }
+
+    function handleMouseDown() {
+        setMouseState('down');
+    }
+
+    function handleMouseUp() {
+        window.removeEventListener('mousemove', mouseMoveListener);
+        setMouseState('none');
+        setPosition({x: 0, y: 0});
     }
 
     return (
         <span
             ref={el}
-            style={{position: 'absolute',top: position.y + 'px', left: position.x + 'px'}}
-            onMouseDown={() => setIsDragged(true)}
-            onMouseUp={() => setIsDragged(false)}
+            className={mouseState === 'dragged' ? 'absolute' : '' + className ?? ''}
+            style={{top:  position.y  + 'px', left: position.x + 'px'}}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
             onMouseMove={(e) => handleMove(e)}
         >
             { children }
