2ffa646c589f13375a7f6cc8241c4cdb1f8343b3
Egor <khromov.developer@mail.ru>
2022-10-06 23:21:11 +0300

    CRUD menu init

---
+++ src/components/CRUD/CRUDMenu.tsx
@@ -0,0 +1,13 @@
+
+
+export default function CRUDMenu() {
+
+
+    return (
+        <div
+            className="fixed left-4 bottom-16 p-4 box-content w-slot h-[60%] bg-neutral-900 opacity-95 rounded-l-xl"
+        >
+            <div className="absolute top-6 left-6"></div>
+        </div>
+    )
+}
