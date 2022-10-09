a614547309381f42e5b3b53a94be0d8322877484
Egor <khromov.developer@mail.ru>
2022-10-09 21:41:32 +0300

    quickfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css'
-import TodoItem from "./components/TodoItem";
-import TodoTable from "./components/TodoTable";
-import DragAndDrop from "./components/DragAndDrop";
-const props = ['First todo', 'Second todo', 'Third todo', 'Fourth todo', 'First todo', 'Second todo', 'Third todo', 'Fourth todo']
+import {RecoilRoot} from "recoil";
+import Roadmap from "./pages/Roadmap";
+
 function App() {
 
   return (
-      <DragAndDrop>
-        <TodoTable title="Do Today!">{props}</TodoTable>
-      </DragAndDrop>
+      <RecoilRoot>
+          <Roadmap></Roadmap>
+      </RecoilRoot>
   )
 }
 
