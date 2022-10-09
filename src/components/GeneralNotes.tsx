9a228fb1d5a36f3666ee8cbd3149a9cc59458e8e
Egor <khromov.developer@mail.ru>
2022-10-09 04:28:14 +0300

    Not using now

---
+++ src/components/GeneralNotes.tsx
@@ -0,0 +1,9 @@
+import Note from "./UI/Note";
+
+
+export default function GeneralNotes() {
+
+    return (
+        <Note>Our project uses this stack: React, Tailwind, Vite, Wouter, React Modules, Our project uses this stack: React, Tailwind, Vite, Wouter, React Modules Our project uses this stack: React, Tailwind, Vite, Wouter, React ModulesOur project uses this stack: React, Tailwind, Vite, Wouter, React Modules</Note>
+    )
+}
