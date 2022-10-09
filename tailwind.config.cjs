ec940f1d181295726ae99635d47afcf35017c7e1
Egor <khromov.developer@mail.ru>
2022-10-09 04:23:21 +0300

    increased slot's height

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,24 @@ module.exports = {
       colors: {
         'primary': '#e0f2fe',
         'secondary': '#7dd3fc',
-        'tertiary': '#4ade80'
+        'tertiary': '#ec4899',
+        'background': '#202020'
+      },
+      width: {
+        'slot': '256px'
+      },
+      height: {
+        'slot': '166px'
+      },
+      animation: {
+        'slide-right': 'slide 500ms ease 1 forward',
+        'slide-left': 'slide 500ms ease 1 reverse backward',
+      },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)'}
+        }
       }
     },
   },
