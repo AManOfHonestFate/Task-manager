bb81f5f2d7033fb355f9a243b12630897a8b9e7f
Egor <khromov.developer@mail.ru>
2022-10-10 22:47:28 +0300

    made sticky

--- src/components/UI/ReturnButton.tsx
+++ src/components/UI/ReturnButton.tsx
@@ -7,10 +7,10 @@ interface ReturnButtonProps {
 export default function ReturnButton({ onClick }: ReturnButtonProps) {
     return (
         <div
-            className="w-8 h-8 border-8 border-t-neutral-100 border-r-neutral-100 relative cursor-pointer
+            className="w-8 h-8 border-8 border-t-neutral-100 border-r-neutral-100 sticky top-4 cursor-pointer
             border-l-transparent border-b-transparent rounded-full rotate-[30] transition-transform before:block
             before:border-x-[1rem] before:border-y-[10px] before:border-r-neutral-100 before:border-y-transparent
-            before:border-l-transparent before:absolute before:bottom-2 before:right-3
+            before:border-l-transparent before:relative before:-top-3 before:-left-7
             hover:-rotate-12 hover:scale-125"
             onClick={onClick}
         ></div>
